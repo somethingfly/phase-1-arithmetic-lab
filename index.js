5b6f9745cb184ac7c94c1838d3a89dea86e25f64
Alfred <somethingfly@somethingfly.com>
2022-06-11 18:14:30 -0700

    commit tree ae404b70ea1dbe1b354fafcc6149847c9244c22d

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+
+const num3 = 16;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,20);
